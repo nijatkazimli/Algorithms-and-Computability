d0840591a969b6b9c1242f4512fd69206ef71bec
nijatkazimliPG <kazimli.n@pg.com>
2024-11-01 14:02:03 +0100

    one file

--- src/dataStructures/Graph.cpp
+++
@@ -1,8 +0,0 @@
-class IGraph {
-public:
-    virtual ~IGraph() = default;
-    virtual int size() const = 0;
-    virtual int hammingDistance(const IGraph& other) const = 0;
-    virtual int maximalCycleLength() const = 0;
-    virtual void minimalExtension() = 0;
-};
--- src/dataStructures/README.md
+++
@@ -1,1 +0,0 @@
-# All data structure related classes here. (like node, root, edge, etc.)
