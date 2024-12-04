27fb4b6202c29640f789347c6da1183eb37f0b6a
Nijat Kazimli <kazimli.n@pg.com>
2024-12-04 18:41:51 +0100

    remove debugging statement, fix output

--- main.cpp
+++ main.cpp
@@ -409,7 +409,7 @@ void Graph::maximalCycleLengthHeuristic() const {
 
     // Explore cycles starting from high-degree nodes
     for (int startNode : nodes) {
-        cout << degrees[startNode] << endl;
+        // cout << degrees[startNode] << endl;
         if (degrees[startNode] < 2) continue; // Skip nodes unlikely to form cycles
         path.clear();
         dfs(startNode, visited, path, maxLength, maxCycles, startNode);
@@ -419,7 +419,7 @@ void Graph::maximalCycleLengthHeuristic() const {
     }
 
     if (maxLength == 0) {
-        cout << red << "NO CYCLES FOUND!" << reset;
+        cout << red << "NO CYCLES FOUND!" << endl << reset;
         chrono::time_point end = chrono::high_resolution_clock::now();
         chrono::duration<double> elapsed = end - start;
         cout << "\tIt took  " << underlineBrightMagenta << elapsed.count()
